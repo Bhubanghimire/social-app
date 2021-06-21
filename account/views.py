5b33673c84629de0f7403d4451cb8f83a39f8ce3
Bhuban Ghimire <bhubanghimire3130.com>
2021-06-21 20:42:36 +0545

    initial commit

---
+++ account/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate,login
+from .forms import LoginForm
+from django.contrib.auth.models import User
+
+# Create your views here.
+def user_login(request):
+    if request.method=='POST':
+        form = LoginForm(request.POST)
+
+
+        if form.is_valid():
+            cd =form.cleaned_data
+            user=authenticate(request,username=cd['username'],password=cd['password'])
+
+            if user is not None:
+                # user= User.objects.get(username=cd['username'],password=cd['password'])
+                
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated ''successfully')
+                else:
+                    return HttpResponse("user desabled")
+            else:
+                return HttpResponse("invalid id password")
+    else:
+        form=LoginForm()
+
+    return render(request,'account/login.html',{'form':form})
